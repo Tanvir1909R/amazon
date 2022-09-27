6458e374b5b4b29bb4cac38c6fc9058d980178a5
Tanvir1909R <trcr719@gmail.com>
2022-09-27 23:57:42 +0600

    finish

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,11 +2,14 @@ import React from "react";
 import './cart.css'
 
 const Cart = ({cart}) => {
+  // console.log(cart);
     let total = 0;
     let shipping = 0;
     let tax = 0;
+    let totalItem = 0;
     for(let c of cart){
-        total = total + +c.price;
+      totalItem += c.quantity;
+        total = total + c.price * totalItem;
         shipping = shipping + +c.shipping;
         tax = +(total * 0.1).toFixed(3)
     }
@@ -14,7 +17,7 @@ const Cart = ({cart}) => {
   return (
     <div className="cart">
       <h1>Order summery</h1>
-      <h3>Select items {cart.length}</h3>
+      <h3>Select items {totalItem}</h3>
       <h3>Total price ${total}</h3>
       <h3>Total shipping ${shipping}</h3>
       <h3>Total Tax ${tax}</h3>
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../cart/Cart';
 import Product from './product/Product';
 import './shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
 
     useEffect(()=>{
         fetch('products.json')
@@ -13,9 +14,34 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[]);
 
-    const handleCart = (product)=>{
-        const newCart = [...cart, product];
+    useEffect(()=>{
+        const storedCart = getStoredCart();
+        let saveCart = [];
+        for(let id in storedCart){
+            const addedProduct = products.find((pro)=> pro.id === id);
+            if(addedProduct){
+                let quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // saveCart.push(addedProduct)  //(same as line 26)
+                saveCart = [...saveCart,addedProduct]
+            }
+        }
+        setCart(saveCart);
+    },[products])
+
+    const handleCart = (selectProduct)=>{
+        const exists = cart.find((product => product.id === selectProduct.id));
+        let newCart = [];
+        if(!exists){
+            selectProduct.quantity = 1;
+            newCart = [...cart, selectProduct]
+        }else{
+            const rest = cart.filter((product => selectProduct.id !== product.id));
+            exists.quantity += 1;
+            newCart = [...rest, exists] 
+        }
         setCart(newCart);
+        addToDb(selectProduct.id);
     }
   return (
     <section id='shop'>
